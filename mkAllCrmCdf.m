5565cf812033b17c6ac25f6a6693e8ec3b7ec396
Chris Piker <cpiker@users.noreply.github.com>
2023-10-19 20:24:01 -0500

    Added main loop in matlab instead of bash

---
+++ mkAllCrmCdf.m
@@ -0,0 +1,22 @@
+function [nFilesGenerated] = mkAllCrmCdf(cInDir, cOutDir)
+%
+% [nFilesGenerated] = mkAllCrmCdf(cInputDir, cOutputDir)
+%
+% Params:
+%   cInDir (char vec)
+%      A location for files named combination_*_L2.mat, where * represents
+%      an integer
+%   cOutDir (char vec)
+%      The location to write the resulting CDF files.
+
+    mkdir(cOutDir);
+
+    % Loop over all files matching the desired pattern
+    aFiles = dir([cInDir filesep 'combination_*_L2.mat']);
+    nWritten = 0;
+    for i = 1:length(aFiles)
+       mkCrmCdf([cInDir filesep aFiles(i).name], cOutDir);
+       nWritten = nWritten + 1;
+    end
+end
+
